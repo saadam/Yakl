cffe71d7b2534fa87f92e1c6d1e403c5b8b429e3
Adam Skorvaga <adam.skorvaga@nazaret.sk>
2025-01-20 22:55:59 +0100

    Design fixes

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Yakl
+
+Yet Another KeeperRL Launcher
+
---
+++ YAKL.LauncherWPF/PathToImageConverter.cs
@@ -0,0 +1,34 @@
+﻿using System.Globalization;
+using System.IO;
+using System.Windows.Data;
+using System.Windows.Media.Imaging;
+
+namespace YAKL.LauncherWPF
+{
+    public class PathToImageConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string path = value as string;
+            if (path != null)
+            {
+                BitmapImage image = new BitmapImage();
+                using (FileStream stream = File.OpenRead(path))
+                {
+                    image.BeginInit();
+                    image.StreamSource = stream;
+                    image.CacheOption = BitmapCacheOption.OnLoad;
+                    image.EndInit(); // load the image from the stream
+                } // close the stream
+                return image;
+            }
+
+            return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
